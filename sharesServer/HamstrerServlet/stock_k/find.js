bf88a41f6be4750b3a8732cec46e702968033b66
zhangcong <826164058@qq.com>
2018-12-26 21:47:05 +0800

    'zhangcong'

---
+++ sharesServer/HamstrerServlet/stock_k/find.js
@@ -0,0 +1,26 @@
+console.log(1)
+MyList[stockName] = MyList[stockName] || mongoose.model(stockName, new Schema({
+    'codeID': String,
+    'K-Lin': Object,
+    'timeRQ': String
+}), stockName);
+if (data.stor) {
+    let query = MyList[stockName].find(data.data)
+    data.stor && query.sort(data.stor)
+    data.limit && query.limit(data.limit)
+    query.exec(function (err, docs) {
+        if(err) {
+            console.log(err);
+            return res.send(err);
+        }
+        return res.send(JSON.stringify(docs));
+    })
+} else {
+    MyList[stockName].find(data, function(err, docs) {
+        if(err) {
+            console.log(err);
+            return res.send(err);
+        }
+        return res.send(JSON.stringify(docs));
+    });
+}
